ee270156f793c377bc3ced92f44f33545c6eee35
infi123 <roei141105@gmail.com>
2023-12-04 20:55:34 +0200

    added enum

--- MMN11/Q2/src/Controller.java
+++ MMN11/Q2/src/Controller.java
@@ -8,6 +8,14 @@ public class Controller {
     private static final int MAX_SHAPE_SIZE = WIDTH / 4;
     private final Random random = new Random();
     private final GraphicsContext gc;
+    private final int NUMBER_OF_SHAPES = 10;
+    private final int NUMBER_OF_SHAPES_TYPES = 3;
+
+    enum ShapeType {
+        RECTANGLE,
+        OVAL,
+        LINE
+    }
 
     public Controller(GraphicsContext gc) {
         this.gc = gc;
@@ -16,20 +24,20 @@ public class Controller {
     public void drawRandomShapes() {
         gc.clearRect(0, 0, WIDTH, HEIGHT);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < NUMBER_OF_SHAPES; i++) {
             gc.setFill(new Color(random.nextDouble(), random.nextDouble(), random.nextDouble(), 1.0));
             int size = random.nextInt(MAX_SHAPE_SIZE);
             int x = random.nextInt(WIDTH);
             int y = random.nextInt(HEIGHT);
 
-            switch (random.nextInt(3)) {
-                case 0:
+            switch (ShapeType.values()[random.nextInt(NUMBER_OF_SHAPES_TYPES)]) {
+                case RECTANGLE:
                     gc.fillRect(x, y, size, size);
                     break;
-                case 1:
+                case OVAL:
                     gc.fillOval(x, y, size, size);
                     break;
-                case 2:
+                case LINE:
                     gc.strokeLine(x, y, x + size, y + size);
                     break;
             }
